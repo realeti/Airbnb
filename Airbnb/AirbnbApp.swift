8017af413b9c4f25e1aa20696cd775c29ef2a1f6
realeti <oro.eto.bristol@gmail.com>
2025-03-13 20:49:16 +0200

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by realeti on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
